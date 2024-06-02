f2a9ba2be3491501fdfcf0f608344b1020c5d017
NicholasTheisen <nicholas.theisen1997@gmail.com>
2024-06-01 20:13:21 -0700

    added games page

--- frontend/gamedb/src/App.js
+++ frontend/gamedb/src/App.js
@@ -5,11 +5,13 @@ import Login from './routes/Login';
 import Register from './routes/Register';
 import Wishlist from './routes/Wishlist';
 import Genre from './routes/Genre'; 
+import Game from './routes/Game';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/games/:id" element={<Game />} /> {/* This will be dynamic */}
         <Route path="/genre/:genre" element={<Genre />} /> {/* This will be dynamic */}
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/register" element={<Register />} />
