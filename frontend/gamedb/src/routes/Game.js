4e959f2814d9653000cce95200f62cd011bfae53
Upgrayedd0408 <michaellowry89@gmail.com>
2024-06-04 17:57:48 -0700

    able to fetch data from backend and display on front end

--- frontend/gamedb/src/routes/Game.js
+++ frontend/gamedb/src/routes/Game.js
@@ -1,28 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import '../styles/Games.scss';
 
 function Game() {
   const { id } = useParams();
   const [game, setGame] = useState(null);
 
   useEffect(() => {
-    const fetchGame = async () => {
-      const response = await fetch(`/api/games/${id}`);
-      const data = await response.json();
-      setGame(data.game);
-    };
-    fetchGame();
+    axios.get(`http://localhost:8080/games/${id}`)
+    .then(res => {setGame(res.data)})
+    .catch(err => console.log(err));
   }, [id]);
+
 
   if (!game) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h2>{game.name}</h2>
-      <p>{game.description}</p>
-      {/* Render other game data as needed */}
+    <div className='games'>
+      <span className="title">{game[0].name}</span>
+      <iframe width="560" height="315" src={game[0].trailer_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
   );
 }
